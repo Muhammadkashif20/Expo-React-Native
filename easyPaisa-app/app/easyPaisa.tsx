168c53c63335b0770284d128e2b0e21ee352b00f
muhammadkashif20 <kashif28@gmail.com>
2024-11-30 10:19:57 +0500

    working on easy paisa home page with routing

--- easyPaisa-app/app/easyPaisa.tsx
+++ easyPaisa-app/app/easyPaisa.tsx
@@ -7,16 +7,14 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Text, View ,StyleSheet } from 'react-native';
-
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -26,24 +24,37 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>EasyPaisa App</Text>
+      {/* Header */}
+      <View style={styles.header}>
+      <MaterialIcons name="account-circle" size={24} color="black" />
+      <Text style={styles.text}>easypaisa</Text>
+      <MaterialIcons name="search" size={24} color="black" />
+      {/* <MaterialIcons name="notifications" size={24} color="black" /> */}
+      </View>
     </View>
   );
-
 }
 const styles=StyleSheet.create({
  container:{
   flex:1,
-  margin:10,
  },
  text:{
   textAlign:'center',
-  fontSize:25,
-  fontWeight:"bold",
- },
+  fontWeight:"600",
+  color:'white',
+  fontSize:20,
+  letterSpacing:1,
 
- 
+ },
+ header:{
+  backgroundColor:'#249A54',
+  height:50,
+  justifyContent:'space-between',
+  flexDirection:'row',
+  paddingVertical:14,
+  paddingHorizontal:14,
+ }
+
 })
--- easyPaisa-app/app/login.tsx
+++ easyPaisa-app/app/login.tsx
@@ -1,16 +1,47 @@
-import { router } from 'expo-router'
-import React from 'react'
-import { View,Text, TouchableOpacity } from 'react-native'
+import { router } from "expo-router";
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const login = () => {
   return (
-    <View>
-    <Text>Welcome To Our Home</Text>
-    <TouchableOpacity onPress={()=>router.push('/login')}>
-        Login
-    </TouchableOpacity>
-</View>
-  )
-}
+    <View style={styles.conatiner}>
+      <View style={styles.header}>
+        <Text style={styles.text}>Your Account Is Login.</Text>
+        <TouchableOpacity
+          onPress={() => router.push("/easyPaisa")}
+          style={styles.btn}
+        >
+          Go To Home
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
-export default login
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1,
+  },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn: {
+    backgroundColor: "#9B00F0",
+    borderRadius: 12,
+    padding: 10,
+    textAlign: "center",
+    color: "#fff",
+    width: 200,
+    fontFamily: "arial",
+  },
+  text: {
+    fontFamily: "arial",
+    textAlign:'center',
+    fontSize:25,
+    fontWeight:"bold",
+    marginVertical:10
+  },
+});
+
+export default login;
